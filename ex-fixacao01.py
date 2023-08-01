71de03335d3178969def06d8486083d7e87b57ce
Felipe Augusto Machado Sampaio Miguel <felipeasmiguel@gmail.com>
2023-08-01 10:49:26 -0300

    Pequeno exércicio de Python.

---
+++ ex-fixacao01.py
@@ -0,0 +1,9 @@
+'''
+Resolva a questão: Fazer um programa que pergunte um valor em dólares e converta para reais. Considere que 1 dólar vale 4,78 reais.
+'''
+
+dolar = float(input("Digite um valor em dólares. $:"))
+
+real = dolar * 4.78
+
+print("O valor em reais equivale a: R$", real)
